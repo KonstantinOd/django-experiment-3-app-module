f7818fd226ec72900ccccb3fedcdd440d1a4964c
Kostiantyn <khoriainov@bewort.com>
2023-11-20 16:16:25 +0200

    feat: updated submodule

--- views.py
+++ views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
 from django.http import HttpResponse
+import datetime
 
 
-def SubmoduleView(request):
-    return HttpResponse({'msg': "remote submodule"})
+def submode(request):
+    return HttpResponse("Hello, I am submodule!")
